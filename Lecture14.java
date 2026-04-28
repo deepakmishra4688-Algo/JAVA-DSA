6fd06fb314db2d7db6b4d60dd43356b55ce020f0
deepakmishra4688-Algo <deepakmishra4688@gmail.com>
2026-04-28 09:12:02 +0530

    Added Loop15.java

--- Lecture14.java
+++ Lecture14.java
@@ -2,6 +2,38 @@ import java . util.*;
 
 public class Lecture14 {
     public static void main (String[] args) {
+        
+        //  Synatax of if statements:
+        // if (boolean expression T or F ) {
+        //     //body
+        // }else{
+        //     //do this 
+        // }
+        
+        int salary = 25400;
+        // if (salary > 10000) {
+        //     salary = salary + 2000;
+        // } else {
+        //     salary = salary + 1000;
+        // }
+        
+        // multiple if- else statements:
+
+        if (salary > 10000) {
+            salary += 2000; // salary = salary +2000
+        } else if (salary > 20000) {
+            salary += 3000;
+
+
+        } else {
+            salary += 1000;
+        }
+
+        System.out.println(salary);
+        
+        
+
+
                             
         
 
